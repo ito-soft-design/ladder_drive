0b60403399ec9890677213bf802bd3fb517239ca
Katsuyoshi Ito <kito@itosoft.com>
2016-04-14 00:26:44 +0900

    refactoring

--- lib/command/escalator.rb
+++ lib/command/escalator.rb
@@ -4,22 +4,13 @@ module Escalator
 
     attr_reader :codes
     
-    def initialize
-      @codes = []
-    end
-    
-    def parse line
-      
-      a = line.split(/\s/)
-      @codes << encode_mnemonic(a[0])
-      @codes.flatten!
-    end
-
-    def encode_mnemonic mnemonic
-      a = []
-      code = mnemonic_dict[mnemonic]
-      a << code if code
-      a
+    def initialize stream
+      @lines = []
+      address = 0
+      stream.each do | line|
+        @lines << AsmLine.new(line, address)
+        address = @lines.last.next_address
+      end
     end
     
     def dump 
@@ -28,8 +19,58 @@ module Escalator
       end
     end
 
+    def dump_line
+      @lines.map do |line|
+        "#{line.address.to_s(16).rjust(4, "0")} #{line.dump_line}"
+      end
+    end
     
     private
+    
+      def parse line
+        @lines << AsmLine.new(line)
+      end
+
+
+  end
+  
+  class AsmLine
+    attr_reader :line
+    attr_reader :codes
+    attr_reader :address
+    
+    def initialize line, address = 0
+      @line = line.upcase.chomp
+      @codes = []
+      @address = address
+      parse
+    end
+    
+    def dump_line
+      "#{dump}\t#{line}"
+    end
+      
+    def dump 
+      @codes.map do |c|
+        c.to_s(16).rjust(2, "0")
+      end
+      .join(" ")
+      .ljust(12)
+    end
+    
+    def next_address
+      address + codes.size
+    end
+
+    private
+    
+      def parse
+        a = line.split(/\s/)
+        c = encode_mnemonic a[0]
+        @codes << c if c
+      end
+      
+
     
       MNEMONIC_DEF = <<EOB
 | |00|10|20|30|40|50|60|70|80|90|A0|B0|C0|D0|E0|F0|
@@ -52,7 +93,7 @@ module Escalator
 |0F|
 EOB
       def mnemonic_dict
-        @mnemonic_dict ||= begin
+        @@mnemonic_dict ||= begin
           h = {}
           MNEMONIC_DEF.dup.split(/\n/)[2..-1].each_with_index do |line, upper|
             line.split(/\|/)[2..-1].each_with_index do |mnemonic, lower|
@@ -66,14 +107,15 @@ EOB
         end
       end
 
+      def encode_mnemonic mnemonic
+        mnemonic_dict[mnemonic]
+      end
+    
   end
-  
+
+
 end
 
 
-asm = Escalator::Asm.new
-
-ARGF.each do |line|
-  asm.parse(line)
-end
-puts asm.dump
+asm = Escalator::Asm.new ARGF
+puts asm.dump_line
