37ac68ab94cda8305b2db18d3431aa11a6ff6ad6
Katsuyoshi Ito <kito@itosoft.com>
2016-10-29 00:51:50 +0900

    refactoring mc protocol

--- lib/escalator/protocols/mitsubishi/mc_protocol.rb
+++ lib/escalator/protocols/mitsubishi/mc_protocol.rb
@@ -23,43 +23,73 @@ module Protocol
         @socket = nil
       end
 
-      def get_bool_value(device)
-        packet = get_packet body_for_get_bit_deivce(device)
+      def get_bit_from_device device
+        get_bits_from_device(1, device).first
+      end
+
+      def get_bits_from_device count, device
+        packet = make_packet(body_for_get_bits_from_deivce(count, device))
         @logger.debug("> #{dump_packet packet}")
         open
         @socket.write(packet.pack("c*"))
         @socket.flush
         res = receive
-        res = res[11]
-        res = (res >> 4) != 0
-        @logger.debug("get #{device.name} => #{res}")
-        res
+        bits = []
+        count.times do |i|
+          v = res[11 + i / 2]
+          if i % 2 == 0
+            bits << ((v >> 4) != 0)
+          else
+            bits << ((v & 0xf) != 0)
+          end
+        end
+        @logger.debug("get #{device.name} => #{bits}")
+        bits
       end
 
-      def set_bool_value(device, value)
-        packet = get_packet body_for_set_bit_device(device, value)
+      def set_bits_to_device bits, device
+        packet = make_packet(body_for_set_bits_device(bits, device))
         @logger.debug("> #{dump_packet packet}")
         open
         @socket.write(packet.pack("c*"))
         @socket.flush
         res = receive
-        @logger.debug("set #{value} => #{device.name}")
+        @logger.debug("set #{bits} to:#{device.name}")
+      end
+      alias :set_bit_to_device :set_bits_to_device
+
+      def get_word_from_device device
+        get_words_from_device(1, device).first
       end
 
-      def get_word_value(device)
-        packet = get_packet body_for_get_word_deivce(device)
+      def get_words_from_device(count, device)
+        packet = make_packet(body_for_get_words_from_deivce(count, device))
         @logger.debug("> #{dump_packet packet}")
         open
         @socket.write(packet.pack("c*"))
         @socket.flush
         res = receive
-        res = res[11] + (res[12] << 8)
-        @logger.debug("get #{device.name} => #{res}")
-        res
+        values = []
+        res[11, 2 * count].each_slice(2) do |pair|
+          values << pair.pack("c*").unpack("v").first
+        end
+        @logger.debug("get from: #{device.name} => #{values}")
+        values
       end
 
-      def set_word_value(device, value)
-        packet = get_packet body_for_set_word_device(device, value)
+      def set_words_to_device values, device
+        packet = make_packet(body_for_set_values_to_device(values, device))
+        @logger.debug("> #{dump_packet packet}")
+        open
+        @socket.write(packet.pack("c*"))
+        @socket.flush
+        res = receive
+        @logger.debug("set #{values} to: #{device.name}")
+      end
+      alias :set_word_to_device :set_words_to_device
+
+      def set_words values, device
+        packet = make_packet(body_for_set_values_to_device(device, values))
         @logger.debug("> #{dump_packet packet}")
         open
         @socket.write(packet.pack("c*"))
@@ -98,23 +128,27 @@ module Protocol
           end
         rescue Timeout::Error
         end
-        @logger.debug("< #{res}")
+        @logger.debug("< #{dump_packet res}")
         res
       end
 
     private
 
-      def get_packet body
+      def make_packet body
         header = [0x50, 0x00,  0x00,  0xff,  0xff, 0x03,  0x00,  0x00, 0x00,  0x10,  0x00]
         header[7..8] = data_for_short(body.length + 2)
         header + body
       end
 
-      def body_for_get_bit_deivce device, count = 1
-        body_for_get_word_deivce device, count, false
+      def body_for_get_bit_from_deivce device
+        body_for_get_bits_from_deivce 1, device
       end
 
-      def body_for_get_word_deivce device, count = 1, word = true
+      def body_for_get_bits_from_deivce count, device
+        body_for_get_words_from_deivce count, device, false
+      end
+
+      def body_for_get_words_from_deivce count, device, word = true
         body = [0x01, 0x04, 0x00, 0x00, 0x00, 0x00, 0x00, 0x90, 0x01, 0x00]
         body[2] = 1 unless word
         body[4..7] = data_for_device(device)
@@ -123,23 +157,25 @@ module Protocol
       end
 
 
-      def body_for_set_bit_device device, value
-        body_for_set_word_device device, value, false
+      def body_for_set_bits_device bits, device
+        body_for_set_values_to_device bits, device, false
       end
+      alias :body_for_set_bit_device :body_for_set_bits_device
 
-      def body_for_set_word_device device, value, word = true
+      def body_for_set_values_to_device values, device, word = true
         body = [0x01, 0x14, 0x00, 0x00, 0x00, 0x00, 0x00, 0x90, 0x01, 0x00]
         d = device
-        values = value.is_a?(Array) ? value : [value]
-        unless word
-          body[2] = 1
+        values = [values] unless values.is_a? Array
+        if word
           values.each do |v|
-            body << (v ? 0x10 : 0x00)
+            body += data_for_short v
             d = d.next_device
           end
         else
-          values.each do |v|
-            body += data_for_short v
+          body[2] = 1
+          values.each_slice(2) do |pair|
+            body << (pair.first ? 0x10 : 0x00)
+            body[-1] |= (pair.last ? 0x1 : 0x00) if pair.size == 2
             d = d.next_device
           end
         end
@@ -155,19 +191,11 @@ module Protocol
       end
 
       def data_for_short value
-        a = []
-        a << (value & 0xff); value >>= 8
-        a << (value & 0xff)
-        a
+        [value].pack("v").unpack("c*")
       end
 
       def data_for_int value
-        a = []
-        a << (value & 0xff); value >>= 8
-        a << (value & 0xff); value >>= 8
-        a << (value & 0xff); value >>= 8
-        a << (value & 0xff)
-        a
+        [value].pack("V").unpack("c*")
       end
 
       def dump_packet packet
--- test/escalator/protocols/mitsubishi/test_mc_protocol.rb
+++ test/escalator/protocols/mitsubishi/test_mc_protocol.rb
@@ -1,11 +1,11 @@
 require 'test/unit'
 require 'escalator'
 
-class TestMcProtoco < Test::Unit::TestCase
+class TestMcProtocol < Test::Unit::TestCase
   include Protocol::Mitsubishi
 
   def setup
-    @protocol = McProtocol.new host:"localhost", port:5010#, log_level: :debug
+    @protocol = McProtocol.new host:"localhost", port:5010, log_level: :debug
     @protocol.open
   end
 
@@ -21,14 +21,28 @@ class TestMcProtoco < Test::Unit::TestCase
 
   def test_set_and_read_bool_value
     d = QDevice.new "M0"
-    @protocol.set_bool_value(d, true)
-    assert_equal true, @protocol.get_bool_value(d)
+    @protocol.set_bit_to_device(true, d)
+    assert_equal true, @protocol.get_bit_from_device(d)
   end
 
   def test_set_and_read_word_value
     d = QDevice.new "D0"
-    @protocol.set_word_value(d, 0x1234)
-    assert_equal 0x1234, @protocol.get_word_value(d)
+    @protocol.set_word_to_device(0x1234, d)
+    assert_equal 0x1234, @protocol.get_word_from_device(d)
+  end
+
+  def test_set_and_read_bits
+    d = QDevice.new "M0"
+    bits = "10010001".each_char.map{|c| c == "1"}
+    @protocol.set_bits_to_device(bits, d)
+    assert_equal bits, @protocol.get_bits_from_device(bits.size, d)
+  end
+
+  def test_set_and_read_words
+    d = QDevice.new "D0"
+    values = (256..265).to_a
+    @protocol.set_word_to_device(values, d)
+    assert_equal values, @protocol.get_words_from_device(values.size, d)
   end
 
 end
