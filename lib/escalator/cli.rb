513e60fb6b3941b3f889c6714d39a949beac5ed7
Katsuyoshi Ito <kito@itosoft.com>
2016-10-30 00:03:38 +0900

    copy plc projects at creation.

--- lib/escalator/cli.rb
+++ lib/escalator/cli.rb
@@ -1,7 +1,7 @@
 # The MIT License (MIT)
-# 
+#
 # Copyright (c) 2016 ITO SOFT DESIGN Inc.
-# 
+#
 # Permission is hereby granted, free of charge, to any person obtaining
 # a copy of this software and associated documentation files (the
 # "Software"), to deal in the Software without restriction, including
@@ -9,10 +9,10 @@
 # distribute, sublicense, and/or sell copies of the Software, and to
 # permit persons to whom the Software is furnished to do so, subject to
 # the following conditions:
-# 
+#
 # The above copyright notice and this permission notice shall be
 # included in all copies or substantial portions of the Software.
-# 
+#
 # THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
 # EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
 # MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
@@ -28,13 +28,14 @@ include FileUtils
 
 module Escalator
   class CLI < Thor
-    
+
     desc "create", "Create a new project"
     def create(name)
       template_path = File.join(Escalator_root, "template", "escalator")
       cp_r template_path, name
-
+      temlate_plc_path = File.join(Escalator_root, "plc")
+      cp_r temlate_plc_path, name
     end
-    
+
   end
 end
