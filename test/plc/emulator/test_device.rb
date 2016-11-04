2dcd77dffaadf4ba897e225688b9aa468d5d72e0
Katsuyoshi Ito <kito@itosoft.com>
2016-11-05 00:31:27 +0900

    DeviceType => Device

--- test/plc/emulator/test_device.rb
+++ test/plc/emulator/test_device.rb
@@ -8,14 +8,14 @@ include Plc::Emulator
 class TestDevice < Test::Unit::TestCase
 
   def test_device
-    d = Device.new DeviceType::X, 1234
-    assert_equal DeviceType::X, d.device_type
+    d = Device.new Device::X, 1234
+    assert_equal Device::X, d.device_type
     assert_equal 1234, d.number
   end
 
   %w(X Y M - C T L SC CC TC D - CS TS H SD).each_with_index do |k, i|
     define_method :"test_#{k}" do
-      assert_equal i, eval("DeviceType::#{k}")
+      assert_equal i, eval("Device::#{k}")
     end unless k == '-'
   end
 
